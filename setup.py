118226590dbd9626314ca1357a529f66cbc85c5d
okay <okay.zed@gmail.com>
2013-10-07 13:27:35 -0700

    0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='KitchenSink',
-    version='0.0.8',
+    version='0.0.9',
     author='okay',
     author_email='okay.zed+kk@gmail.com',
     packages=['kitchen_sink' ],
